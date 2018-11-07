3bffe2059be83b9f2602ec5b717d6cb0c75f168c
Jan Sobczynski <jan.sobczynski@pl.ibm.com>
2018-11-07 11:40:02 +0100

    changes

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const app = express();
 const passportSetup = require('./congif/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./congif/keys');
 
 // set up view engine
 app.set('view engine', 'ejs');
 
+// connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () => {
+    console.log('connected to mongodb');
+})
 // set up routes
 app.use('/auth', authRoutes);
 
--- congif/keys.js
+++ congif/keys.js
@@ -3,5 +3,8 @@ module.exports = {
     google: {
         clientID: '761653063610-rsevi72bugie4qkfq3aq39qbo84ts8mr.apps.googleusercontent.com',
         clientSecret: 'ttaJIGjL9jpE5GQApxI6vXv_'
+    },
+    mongodb: {
+        dbURI: 'mongodb://netninja:123Jan@ds253203.mlab.com:53203/nn-oauth'
     }
 }; 
