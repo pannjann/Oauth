37b728e42e85dfe761fbbd7fc0b09d997b0d3828
Jan Sobczynski <jan.sobczynski@pl.ibm.com>
2018-11-07 10:18:16 +0100

    changes

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
-
+const authRoutes = require('./routes/auth-routes');
 const app = express();
 
 // set up view engine
-
 app.set('view engine', 'ejs');
 
-// create home route
+// set up routes
+app.use('/auth', authRoutes);
 
+// create home route
 app.get('/',(req,res) => {
     res.render('home');
 });
