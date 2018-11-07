774e8ff70082316c11aede3d936f010c348668cf
Jan Sobczynski <jan.sobczynski@pl.ibm.com>
2018-11-07 12:41:40 +0100

    cgange

---
+++ config/passport-setup.js
@@ -0,0 +1,32 @@
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./keys');
+const User = require('../models/user-model');
+
+passport.use(
+    new GoogleStrategy({
+        // options for google strategy
+        callbackURL: '/auth/google/redirect',
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+        // check if user already exists in our db
+
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser){
+                // already have the user
+                console.log('user is:', currentUser);
+            } else {
+                // if not create user in our db
+                new User({
+                    username: profile.displayName,
+                    googleId: profile.id
+                }).save().then((newUser) => {
+                    console.log('new user created: ' + newUser);
+                });
+            }
+        })
+
+    })
+);
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,24 +2,25 @@ const router = require('express').Router();
 const passport = require('passport');
 
 // auth login
-router.get('/login',(req,res) => {
-    res.render('login');
+router.get('/login', (req, res) => {
+    res.render('login', { user: req.user });
 });
 
-//auth logout
-router.get('/logout',(req,res) => {
+// auth logout
+router.get('/logout', (req, res) => {
     // handle with passport
-    res.send('logging out')
+    res.send('logging out');
 });
 
-// auth with google
-router.get('/google',passport.authenticate('google',{
+// auth with google+
+router.get('/google', passport.authenticate('google', {
     scope: ['profile']
 }));
 
-
 // callback route for google to redirect to
-router.get('/google/redirect', passport.authenticate('google'),(req,res) => {
-    res.send('you reached the callback URI');
+// hand control to passport to use code to grab profile info
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+    res.send('you reached the redirect URI');
 });
+
 module.exports = router;
