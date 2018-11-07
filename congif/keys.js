fc3ba78665f568ebc3426c3cb32e6e31c7f91b55
Jan Sobczynski <jan.sobczynski@pl.ibm.com>
2018-11-07 11:17:50 +0100

    add more

---
+++ congif/keys.js
@@ -0,0 +1,7 @@
+// add this file to .gitignore
+module.exports = {
+    google: {
+        clientID: '761653063610-rsevi72bugie4qkfq3aq39qbo84ts8mr.apps.googleusercontent.com',
+        clientSecret: 'ttaJIGjL9jpE5GQApxI6vXv_'
+    }
+}; 
